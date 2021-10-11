8aa881df6ef6854a29ca28790d0f8e62ee2d803a
Akash <akashstunes@gmail.com>
2021-10-11 11:58:51 +0530

    added some mobile resonsive for home page need to add for footer

--- JsFiles/common.js
+++ JsFiles/common.js
@@ -1,1 +1,15 @@
-console.log('hello')
+// nav bar function
+
+let menu  = document.querySelector(".nav_group");
+
+let handburger = document.querySelector(".handburger");
+
+
+
+
+handburger.addEventListener('click',()=>{
+    menu.classList.toggle(
+        "open"
+    )
+
+})
